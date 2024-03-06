4e03123b3d438f51c9cb98bee725f997258e7618
surgexuan1208 <jeffs941208@gmail.com>
2024-03-06 15:00:30 +0800

    Subscriber端

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using System.Net.Mqtt;
 using System.ServiceModel.Channels;
 using System.Net.Mqtt.Sdk;
+using System.Threading;
 
 namespace MQTT
 {
@@ -27,7 +28,7 @@ namespace MQTT
         {
             InitializeComponent();
         }
-
+        static CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
             if (Topic.Text.Length <1 ||Topic.Text.Length==0)
@@ -64,6 +65,58 @@ namespace MQTT
             {
                 MessageBox.Show($"錯誤：{ex.Message}");
             }
+        }
+
+        private async void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Topic2.Text.Length < 1 || Topic2.Text.Length == 0)
+            {
+                MessageBox.Show("主題不能為空");
+                return;
+            }
+            try
+            {
+                // 建立 MQTT 客戶端配置
+                var configuration = new MqttConfiguration
+                {
+                    BufferSize = 65536,
+                    Port = int.Parse(port2.Text),
+                    KeepAliveSecs = 10,
+                    WaitTimeoutSecs = 2,
+                    MaximumQualityOfService = MqttQualityOfService.AtMostOnce,
+                    AllowWildcardsInTopicFilters = true
+                };
+                // 建立 MQTT 客戶端
+                var client = await MqttClient.CreateAsync((string)ip2.Text, configuration);
+                MessageBox.Show("已建立 MQTT 客戶端！");
+                // 連接到 MQTT 伺服器
+                var sessionState = await client.ConnectAsync(new MqttClientCredentials(clientId: ClientID2.Text), cleanSession: true);
+                MessageBox.Show("已連接到 MQTT 伺服器！");
+                await client.SubscribeAsync(Topic2.Text, MqttQualityOfService.AtMostOnce); //QoS0
+                client.MessageStream.Subscribe(async msg =>
+                {
+                    // 將收到的消息顯示在 UI 中
+                    Dispatcher.Invoke(() =>
+                    {
+                        ReceivedMessagesTextBox.AppendText($"收到來自主題 '{msg.Topic}' 的消息：{Encoding.UTF8.GetString(msg.Payload)}\n");
+                    });
+                    if (cancellationTokenSource.IsCancellationRequested)
+                    {
+                        // 斷開 MQTT 連接
+                        await client.DisconnectAsync();
+                        MessageBox.Show("已斷開 MQTT 連接！");
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"錯誤：{ex.Message}");
+            }
+        }
+        // 斷開連接
+        private async void StopButton_Click(object sender, EventArgs e)
+        {
+            cancellationTokenSource.Cancel();
         }
     }
 }
